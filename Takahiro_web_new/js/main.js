5fd22a84ac16d41bffd09c38adc2614501a1ab13
Mitsuki Nakamura <meow777suki@gmail.com>
2018-08-28 21:52:24 +0900

    hello
    yay

---
+++ Takahiro_web_new/js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    console.log('read');
+
+    $('.slider').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        // adaptiveHeight: true,
+        dots: false,
+        fade: true,
+        pauseOnHover: false
+    });
+
+});
